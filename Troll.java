60855114bbe50bcd83b6b85168c14c0fb12ddff1
ZJackie <zjackiew@gmail.com>
2013-11-16 22:27:08 -0500

    Update Troll.java

--- Troll.java
+++ Troll.java
@@ -1,4 +1,21 @@
 public class Troll extends Character{
+  
+    public Troll (){
+        _health = 200;
+        _strength = 80;
+        _defense = 30;
+        _attackRating = 1;
+        _speed = 1;}
+
+        public String getName(){
+                return "Jabberwocky";}
+                
+        public void specialize() {}
+        
+		    public void normalize(){}
+    
+        public static abstract String about(){
+                return "*placeholder*";}
 
 ░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░ 
 ░░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░░ 
