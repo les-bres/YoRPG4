b2da6ce4cb668f39c6d9fae58f8db7eb7661dbd2
Fitzy97 <seanfitzgerald321@gmail.com>
2013-11-17 13:32:29 -0500

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -1,54 +1,49 @@
-
-
 public class Warrior extends Character{
     
     // attributes:
-
     private String _name;
     
-
-
     // constructor
     public Warrior() {
- 	_name = "no name";
-	_health = 125;
-	_strength = 100;
-	_defense = 40;
-	_attackRating = .4;
-	_speed = 5;
+         _name = "no name";
+        _health = 125;
+        _strength = 100;
+        _defense = 40;
+        _attackRating = .4;
+        _speed = 5;
     }
 
     public Warrior( String newName) {
-	this();
- 	_name = newName;
+        this();
+         _name = newName;
     }
 
     
     public String getName() {
-	return _name;
+        return _name;
     }
     
 
     public void specialize() {
-	_defense -= 1;
-	_attackRating += .1;
+        _defense -= 1;
+        _attackRating += .1;
     }
 
     public void normalize() {
-	_defense = 40;
-	_attackRating = .4;
-    }
-
-    public String about() {
-    return "Warriors are those of virtue, who exhibit their courage and integrity through their commitment to eradicating all evils from the face of the earth.";
+        _defense = 40;
+        _attackRating = .4;
     }
     
     public void tame( Pegasus target) {
-    	if ( (int) (Math.random() * 2) == 0 ) {
-    		target.tameMe();
-    		_speed += 2;
-    		_attackRating += .2;
-    	}
+            if ( (int) (Math.random() * 2) == 0 ) {
+                    target.tameMe();
+                    _speed += 2;
+                    _attackRating += .2;
+            }
+    }
+
+    public String about() {
+	return "A warrior is brave, strong, and intelligent.  He slays mighty \ndragons and rescues princesses from high towers.  He dabbles in the art of \ncomputer science and takes pleasure in writing code that promotes his image, \nusually in the form of a role playing game.  He likes copmuter games almost as\n much as his mighty sword.";
     }
 
 }
