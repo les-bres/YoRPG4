582a57254d3dfbc49388e7899a8f8cc14d4d4676
Fitzy97 <seanfitzgerald321@gmail.com>
2013-11-17 13:29:45 -0500

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -1,49 +1,33 @@
-// Leslie Bresnahan
-// pd 9
-// HW # 25
-// 2013-11-14
-
-
 public class Monster extends Character{
     
 
     // constructor
     public Monster() {
-	_health = 150;
-	_strength =  ((int) (Math.random() * 45)) + 20;
-	_defense = 20;
-	_attackRating = 1;
-	_speed = 3;
-    }
-
-    public String about() {
-    return "Monsters are those who lurk in shadows, seeking to inflict harm and degrade the world from inside to out.";
+        _health = 150;
+        _strength =  ((int) (Math.random() * 45)) + 20;
+        _defense = 20;
+        _attackRating = 1;
+        _speed = 3;
     }
 
 
     // override abstract methods
     public String getName() {
-	return "Monsters do not have names.";
+        return "Monsters do not have names.";
     }
 
     public void normalize() {}
     public void specialize() {}
 
-    public static void main( String[] args ) {
-	Monster mike = new Monster();
-
-	System.out.println( "Is Alive?" );
-	System.out.println( mike.isAlive() );
-
-	System.out.println( "Defense?" );
-	System.out.println( mike.getDefense() );
-
-	System.out.println( "Name?" );
-	System.out.println( mike.getName() );
-
-	System.out.println( "About?" );
-	System.out.println( mike.about() );
-
+    public String about() {
+	return "A monster is the most misunderstood being in the realm.  He constantly " + 
+	"tries to befriend humans, but can never get his message across quite right. " + 
+	"Most monsters do not speak English or any language whatsoever, thus his cries " + 
+	"for love come across as ferocious growls and, in some cases, fire-breath.  Even " + 
+	"with their most good-hearted attempts at provoking a loving relationship between " + 
+	"him and the humans, he always ends up destroying a village or kidnapping \na maiden.  " + 
+	"Monsters are said to be mean, destructive, and scary, but the truth is that they " + 
+	"are clumsy and unintelligable, but have hearts of gold.";
     }
 
 }
