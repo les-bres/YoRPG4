37a0e0c9830d668101690c548886895c4105c378
Fitzy97 <seanfitzgerald321@gmail.com>
2013-11-17 13:31:06 -0500

    Update Rouge.java

--- Rouge.java
+++ Rouge.java
@@ -1,43 +1,44 @@
 public class Rogue extends Character {
   
-  private String _name;
+    private String _name;
 
-  public Rogue() {
-    _health = 75;
-    _attackRating = 0.3;
-    _defense = 50;
-    _strength = 60;
-    _speed = 7;
-  }
+    public Rogue() {
+	_health = 75;
+	_attackRating = 0.3;
+	_defense = 50;
+	_strength = 60;
+	_speed = 7;
+    }
   
-  public Rogue( String name ) {
-    this();
-    _name = name;
-  }
+    public Rogue( String name ) {
+	this();
+	_name = name;
+    }
   
-  public void takeHealth( Character victim ) {
-    int damage = (int) (_strength * attackRating) - victim.getDefense();
-    _health += damage;
-  }
+    public void takeHealth( Character victim ) {
+	int damage = (int) (_strength * _attackRating) - victim.getDefense();
+	_health += damage;
+    }
   
-  public String getName() {
-    return _name;
-  }
+    public String getName() {
+	return _name;
+    }
   
-  public void specialize() {
-    _attackRating += .1;
-    _defense -= 1;
-  }
+    public void specialize() {
+	_attackRating += .1;
+	_defense -= 1;
+    }
   
-  public void normalize() {
-    _attackRating -= .1;
-    _defense +=1;
-  }
+    public void normalize() {
+	_attackRating -= .1;
+	_defense +=1;
+    }
   
-  public String about() {
-    return "Rogues are seldom seen or heard, but they are nevertheless a constant annoyance to others." +
-    "  They have a passion for stealing, and are very good at it, as their stealth is unmatched by other creatures" + 
-    "  Although they are never achieve the fame of a warrior, a rogue is a significant adversary with many tricks up his sleave.";
-  }
+    public String about() {
+	return "Rogues are seldom seen or heard, but they are nevertheless a constant annoyance to others." +
+	    "  They have a passion for stealing, and are very good at it, as their stealth is unmatched by other creatures" + 
+	    "  Although they are never achieve the fame of a warrior, a rogue is a significant adversary with many tricks up his sleave.";
+    }
   
 }
+
