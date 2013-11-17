e908ffa6b92628fa524908fab4d008ed682a6f86
Fitzy97 <seanfitzgerald321@gmail.com>
2013-11-17 13:24:28 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,49 +1,48 @@
-
 public abstract class Character {
 
     // note protected instance variables so subclasses can access:
-    protected int _health, _strength, _defense,;
-    protected double _attackRating, _speed;
+    protected int _health, _strength, _defense, _speed;
+    protected double _attackRating;
 
 
     public boolean isAlive() {
-	return (_health > 0);
+        return (_health > 0);
     }
 
 
     public int getDefense() {
-	return _defense;
+        return _defense;
     }
     
     public int getSpeed() {
-    	return _speed;
+	return _speed;
     }
     
     public int getHealth() {
-    	return _health;
+	return _health;
     }
 
-     public void lowerHP(int decreaser) {
-	if (_health > decreaser) {
-	    _health -= decreaser;
-	}
-	else {
-	    _health = 0;
-	}
+    public void lowerHP(int decreaser) {
+        if (_health > decreaser) {
+            _health -= decreaser;
+        }
+        else {
+            _health = 0;
+        }
     }
 
 
     // Character is parameter type so both monsters and warriors can be accepted
 
     public int attack( Character victim ) {
-	int Damage = (int) ( (_strength * _attackRating) - victim.getDefense() );
-	if (Damage < 0) {
-		Damage = 0;
-	}
-	if (Math.random() * 10.0 > victim.getSpeed()) {
-		victim.lowerHP( Damage );
-	}
-	return Damage;
+        int Damage = (int) ( (_strength * _attackRating) - victim.getDefense() );
+        if (Damage < 0) {
+	    Damage = 0;
+        }
+        if (Math.random() * 10.0 > victim.getSpeed()) {
+	    victim.lowerHP( Damage );
+        }
+        return Damage;
     }
 
 
