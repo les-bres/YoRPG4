0d83b62e2da448a018661cb7c1cd2a42be11d698
ZJackie <zjackiew@gmail.com>
2013-11-16 22:22:43 -0500

    Create Goblin.java

---
+++ Goblin.java
@@ -0,0 +1,24 @@
+public class Goblin extends Character{
+
+    public Goblin (){
+        _health = 200;
+        _strength = 10;
+        _defense = 10;
+        _attackRating = 10;
+        _speed = 10;}
+
+        public String getName(){
+                return "Goblin Swarm";}
+                
+        public void specialize() {}
+        
+	    	public void normalize(){}
+    
+        public static abstract String about(){
+                return "*placeholder*";}
+
+
+        public void swarm (){
+        _attackRating = _health / 20;}
+  
+}
