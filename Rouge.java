8f85e7faee31c7d041bf66898988d182353af119
Fitzy97 <seanfitzgerald321@gmail.com>
2013-11-16 20:48:59 -0500

    Update Rouge.java

--- Rouge.java
+++ Rouge.java
@@ -1,8 +1,16 @@
 public class Rogue {
 
   public Rogue() {
-    _health = ;
-    _attackRating = ;
-    _defense = ;
-    
+    _health = 75;
+    _attackRating = 0.3;
+    _defense = 50;
+    _strength = 60;
+    _speed = 100;
   }
+  
+  public Rogue( String name ) {
+    this();
+    _name = name;
+  }
+  
+  public 
