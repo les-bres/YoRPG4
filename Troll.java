aa183e28ecc4602bc28aa594ffccd3453abb16bc
ZJackie <zjackiew@gmail.com>
2013-11-17 20:49:33 -0500

    Update Troll.java

--- Troll.java
+++ Troll.java
@@ -31,6 +31,7 @@ public class Troll extends Character{
         if (Math.random() * 10.0 > victim.getSpeed()) {
             victim.lowerHP( Damage );
         }
+       	victim.lowerHP (Damage);
     }
         
     public void club(Character victim) {
